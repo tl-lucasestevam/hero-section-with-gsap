6451b53a78601466a87661df3dde1b37b33f4e90
Lucas Estevam <dev.lucasestevam@gmail.com>
2021-12-30 07:09:31 -0300

    feat: Add hero animations with gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,94 @@
+import { TimelineLite, TweenMax, Power3 } from "gsap";
+
 import "./styles/App.scss";
 
 import arrow from "./assets/arrow-right.svg";
 import imgGirl from "./assets/girl.webp";
 import imgBoy from "./assets/boy.webp";
+import { useEffect, useRef } from "react";
 
 function App() {
+  let app = useRef(null);
+  let images = useRef(null);
+  let content = useRef(null);
+
+  let tl = new TimelineLite({ delay: 0.8 });
+
+  useEffect(() => {
+    // Images vars
+    const girlImage = images.firstElementChild;
+    const boyImage = images.lastElementChild;
+
+    // Content vars
+    const headLineFirst = content.children[0].children[0];
+    const headLineSecond = headLineFirst.nextSibling;
+    const headLineThird = headLineSecond.nextSibling;
+    const contentParagraph = content.children[1];
+    const contentButton = content.children[2];
+
+    //Remove initial flash
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+
+    //Images Animation
+    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut }, "Start")
+      .from(
+        girlImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      )
+      .from(boyImage, 1.4, { y: 1280, ease: Power3.easeOut }, 0.2)
+      .from(
+        boyImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      );
+
+    //Content Animation
+    tl.staggerFrom(
+      [headLineFirst.children, headLineSecond.children, headLineThird.children],
+      1,
+      {
+        y: 44,
+        ease: Power3.easeOut,
+        delay: 0.8,
+      },
+      0.15,
+      "Start"
+    )
+      .from(
+        contentParagraph,
+        1,
+        { y: 20, opacity: 0, ease: Power3.easeOut },
+        1.4
+      )
+      .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6);
+  });
+
   return (
-    <div className="hero">
+    <div ref={(element) => (app = element)} className="hero">
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
-            <div className="hero-content-inner">
+            <div
+              className="hero-content-inner"
+              ref={(element) => (content = element)}
+            >
               <h1>
-                <span className="hero-content-line">
-                  <span className="hero-content-line-inner">
-                    Relieving the burden <br />
-                  </span>
-                </span>
-                <span className="hero-content-line">
-                  <span className="hero-content-line-inner">
-                    of disease caused <br />
-                  </span>
-                </span>
-                <span className="hero-content-line">
-                  <span className="hero-content-line-inner">by behaviors.</span>
-                </span>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">
+                    Relieving the burden
+                  </div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">
+                    of disease caused
+                  </div>
+                </div>
+                <div className="hero-content-line">
+                  <div className="hero-content-line-inner">by behaviors.</div>
+                </div>
               </h1>
               <p>
                 Better treats serious cardiometabolic diseases to transform
@@ -42,7 +106,10 @@ function App() {
             </div>
           </div>
           <div className="hero-images">
-            <div className="hero-images-inner">
+            <div
+              className="hero-images-inner"
+              ref={(element) => (images = element)}
+            >
               <div className="hero-image girl">
                 <img src={imgGirl} alt="girl" />
               </div>
